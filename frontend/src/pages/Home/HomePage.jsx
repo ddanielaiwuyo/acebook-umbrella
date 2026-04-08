695f4236d448bbc38bda70cc66660c3b9152c9c5
jessica.tsolova <jessica.tsolova@moonpig.com>
2026-04-08 14:45:49 +0100

    WIP: saving progress before trying new changes

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -5,9 +5,23 @@ import "./HomePage.css";
 export function HomePage() {
   return (
     <div className="home">
-      <h1>Welcome to Acebook!</h1>
-      <Link to="/signup">Sign Up</Link>
-      <Link to="/login">Log In</Link>
+      <div className="left-container">
+        {/* Empty for now, I will look into adding an image later */}
+      </div>
+
+      <div className="right-container">
+        <div className="home-content">
+          <h1>Welcome to Acebook</h1>
+          <div className="buttons">
+            <Link to="/signup" className="btn">
+              Sign Up
+            </Link>
+            <Link to="/login" className="btn">
+              Log In
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
