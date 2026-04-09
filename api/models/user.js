ae224d7abc7089de40313c509671a013ae983c65
Ameerah Adisa <ameeygold@gmail.com>
2026-04-09 18:12:26 +0100

    Updated the user schema

--- api/models/user.js
+++ api/models/user.js
@@ -1,8 +1,43 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
+	profileImg: { type: String },
+	email: { type: String, required: true },
+	password: { type: String, required: true },
+	friends: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+		},
+	],
+	sentFriendRequests: [
+		{
+			user: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "User",
+			},
+			status: {
+				type: String,
+				enum: ["pending", "accepted", "rejected"],
+				default: "pending",
+			},
+		},
+	],
+	receivedFriendRequests: [
+		{
+			user: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "User",
+			},
+			status: {
+				type: String,
+				enum: ["pending", "accepted", "rejected"],
+				default: "pending",
+			},
+		},
+	],
 });
 
 const User = mongoose.model("User", UserSchema);
