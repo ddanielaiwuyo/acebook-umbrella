f7064a74ea5b79a778da50f8b5d3c7902c776ffa
Ameerah Adisa <ameeygold@gmail.com>
2026-04-09 18:13:43 +0100

    Added the add friend button component

--- frontend/src/components/AddButton.jsx
+++ frontend/src/components/AddButton.jsx
@@ -1,7 +1,14 @@
-export function AddButtonb () {
+import { addFriend } from "../services/friends";
+
+export function AddButton(props) {
+	async function handleClick() {
+		await addFriend(props.userId)
+		props.onAdd(props.userId)
+	}
+
 	return (
 		<div>
-			<button>Add Friend</button>
+			<button onClick={handleClick}>Add Friend</button>
 		</div>
 	);
 }
