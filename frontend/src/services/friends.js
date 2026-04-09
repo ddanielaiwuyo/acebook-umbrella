bb2a709427f4a555bac8f4125c49773cd2e56920
Ameerah Adisa <ameeygold@gmail.com>
2026-04-09 18:22:36 +0100

    Added friends.js to the services for the frontend

---
+++ frontend/src/services/friends.js
@@ -0,0 +1,133 @@
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+export async function addFriend(userId) {
+	const token = localStorage.getItem("token");
+	const requestOptions = {
+		method: "POST",
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	};
+
+	const response = await fetch(
+		`${BACKEND_URL}/friends/${userId}`,
+		requestOptions,
+	);
+
+	if (response.status !== 200) {
+		throw new Error("Unable to add friend");
+	}
+
+	const data = await response.json();
+	return data;
+}
+
+export async function getFriends() {
+	const token = localStorage.getItem("token");
+	const requestOptions = {
+		method: "GET",
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	};
+
+	const response = await fetch(`${BACKEND_URL}/friends/`, requestOptions);
+
+	const data = await response.json();
+	return data;
+}
+
+//This service gets other users to be added to people you may know
+export async function getOtherUsers() {
+	const token = localStorage.getItem("token");
+	const requestOptions = {
+		method: "GET",
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	};
+
+	const response = await fetch(
+		`${BACKEND_URL}/friends/other-users`,
+		requestOptions,
+	);
+
+	const data = await response.json();
+	return data;
+}
+
+//This service gets receieved friend requests
+export async function getFriendRequests() {
+	const token = localStorage.getItem("token");
+	const requestOptions = {
+		method: "GET",
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	};
+
+	const response = await fetch(
+		`${BACKEND_URL}/friends/friend-requests`,
+		requestOptions,
+	);
+
+	const data = await response.json();
+	return data;
+}
+
+//This service accepts friend requests
+export async function acceptFriendRequest(userId) {
+	const token = localStorage.getItem("token");
+	const requestOptions = {
+		method: "PATCH",
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	};
+
+	const response = await fetch(
+		`${BACKEND_URL}/friends/${userId}/accept`,
+		requestOptions,
+	);
+
+	const data = await response.json();
+	return data;
+}
+
+//This service deletes friend requests
+export async function deleteFriendRequest(userId) {
+	const token = localStorage.getItem("token");
+	const requestOptions = {
+		method: "PATCH",
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	};
+
+	const response = await fetch(
+		`${BACKEND_URL}/friends/${userId}/delete`,
+		requestOptions,
+	);
+
+	const data = await response.json();
+	return data;
+}
+
+//This service removes friends
+export async function removeFriend(userId) {
+	const token = localStorage.getItem("token");
+	const requestOptions = {
+		method: "PATCH",
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	};
+
+	const response = await fetch(
+		`${BACKEND_URL}/friends/${userId}/remove`,
+		requestOptions,
+	);
+
+	const data = await response.json();
+	return data;
+}
