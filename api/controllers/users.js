d4549efb39e5ab0356c954fe68ccec171eb90c3c
ddanielaiwuyo <aiwuyoeghosadaniel2@gmail.com>
2026-04-09 16:11:15 +0100

    feat: used bcrypt for signup page and updated signup page to have errors

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,24 +1,63 @@
+const bcrypt = require("bcrypt")
 const User = require("../models/user");
 
-function create(req, res) {
-  const email = req.body.email;
-  const password = req.body.password;
+const EMAIL_MIN_LENGTH = 5;
+const PASSWORD_MIN_LENGTH = 8;
+const SALT_ROUNDS = 10;
 
-  const user = new User({ email, password });
-  user
-    .save()
-    .then((user) => {
-      console.log("User created, id:", user._id.toString());
-      res.status(201).json({ message: "OK" });
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(400).json({ message: "Something went wrong" });
-    });
+async function create(req, res) {
+	try {
+		const { firstName, lastName, email, password, } = req.body
+		const user = new User({ email, password });
+		if (!email || !password) {
+			res.status(400).json({ message: "Invalid credentails, email and password must not be empty" })
+			return
+		}
+
+		if (!emailIsValid(email)) {
+			res.status(400).json({ message: "Invalid email address" })
+			return
+		}
+
+		if (password.trim().length < PASSWORD_MIN_LENGTH) {
+			console.log("password too short", password, password.length)
+			res.status(400).json({ message: `Password too short, must be at least ${PASSWORD_MIN_LENGTH}` })
+			return
+		}
+
+		// Now check if user with that email already exists
+		const userExists = await User.findOne({ email: email })
+		if (userExists) {
+			console.info(`User with email: ${email} already exists `)
+			res.status(409).json({ message: `User with email ${email} already exists` })
+			return
+		}
+
+		const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
+		const newUser = new User({ email: email, password: hashedPassword })
+		await newUser.save()
+
+		console.info(`User with email: ${email} saved successfully`)
+		res.status(201).json({ message: "Created successfully" })
+
+	} catch (err) {
+		console.error("Could not create user")
+		console.error(err)
+		res.status(500).json({ message: "Service is down, please try again later" });
+	}
+}
+
+function emailIsValid(email) {
+	let formattedEmail = email.trim().toLowerCase()
+	if (formattedEmail.length >= EMAIL_MIN_LENGTH && formattedEmail.includes("@")) {
+		return true
+	}
+
+	return false
 }
 
 const UsersController = {
-  create: create,
+	create: create,
 };
 
 module.exports = UsersController;
--- frontend/src/services/authentication.js
+++ frontend/src/services/authentication.js
@@ -2,62 +2,61 @@
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 if (!BACKEND_URL) {
-  throw new Error(`
+	throw new Error(`
 		Could not find backend url to send requests to.
 		Found ${BACKEND_URL}, Have you added it to the .env file?
 		`);
 }
 
 export async function login(email, password) {
-  const payload = {
-    email: email,
-    password: password,
-  };
+	const payload = {
+		email: email,
+		password: password,
+	};
 
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  };
+	const requestOptions = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(payload),
+	};
 
-  const response = await fetch(`${BACKEND_URL}/tokens`, requestOptions);
+	const response = await fetch(`${BACKEND_URL}/tokens`, requestOptions);
 
-  // docs: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/201
-  if (response.status === 201) {
-    let data = await response.json();
-    console.log("Login response -> ", data);
-    return data.token;
-  } else {
-    throw new Error(
-      `Received status ${response.status} when logging in. Expected 201`,
-    );
-  }
+	// docs: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/201
+	if (response.status === 201) {
+		let data = await response.json();
+		console.log("Login response -> ", data);
+		return data.token;
+	} else {
+		throw new Error(
+			`Received status ${response.status} when logging in. Expected 201`,
+		);
+	}
 }
 
 export async function signup(email, password) {
-  const payload = {
-    email: email,
-    password: password,
-  };
+	try {
+		const payload = {
+			email: email,
+			password: password,
+		};
 
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  };
+		const requestOptions = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(payload),
+		};
 
-  let response = await fetch(`${BACKEND_URL}/users`, requestOptions);
+		const response = await fetch(`${BACKEND_URL}/users`, requestOptions);
+		const data = await response.json()
 
-  // docs: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/201
-  if (response.status === 201) {
-    return;
-  } else {
-    throw new Error(
-      `Received status ${response.status} when signing up. Expected 201`,
-    );
-  }
+		return { message: data.message, status: response.status }
+	} catch (err) {
+		console.error(err)
+		return { message: "Service is down, please try again later", status: 500 }
+	}
 }
