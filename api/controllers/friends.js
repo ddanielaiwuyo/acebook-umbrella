39645fd68f3353982d76a38a1874dc0c8fae9077
Ameerah Adisa <ameeygold@gmail.com>
2026-04-09 18:07:04 +0100

    Added friends.js to the controllers

---
+++ api/controllers/friends.js
@@ -0,0 +1,186 @@
+const User = require("../models/user");
+
+async function addFriendRequest(req, res) {
+	try {
+		const senderId = req.user_id;
+		const receiverId = req.params.id;
+		const sender = await User.findById(senderId);
+		const receiver = await User.findById(receiverId);
+		const alreadySent = sender.sentFriendRequests.find(
+			(request) => request.user.toString() === receiverId,
+		);
+		if (alreadySent) {
+			return res.status(400).json({ message: "Friend request already sent" });
+		}
+		sender.sentFriendRequests.push({
+			user: receiverId,
+			status: "pending",
+		});
+
+		receiver.receivedFriendRequests.push({
+			user: senderId,
+			status: "pending",
+		});
+
+		await sender.save();
+		await receiver.save();
+
+		res.status(200).json({ message: "Friend request sent successfully" });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+}
+
+//This function is for accepting friend requests
+async function acceptFriendRequest(req, res) {
+	try {
+		const receiverId = req.user_id;
+		const senderId = req.params.id;
+
+		const receiver = await User.findById(receiverId);
+		const sender = await User.findById(senderId);
+		const alreadyFriends = receiver.friends.find(
+			(id) => id.toString() === senderId,
+		);
+		if (alreadyFriends) {
+			return res.status(400).json({ message: "Already friends" });
+		}
+		receiver.friends.push(senderId);
+
+		sender.friends.push(receiverId);
+
+		sender.sentFriendRequests.pull({
+			user: receiverId,
+		});
+
+		receiver.receivedFriendRequests.pull({
+			user: senderId,
+		});
+
+		await sender.save();
+		await receiver.save();
+
+		res.status(200).json({ message: "Friend request accepted successfully" });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+}
+
+//This function is for deleteing friend requests
+async function deleteFriendRequest(req, res) {
+	try {
+		const receiverId = req.user_id;
+		const senderId = req.params.id;
+
+		const receiver = await User.findById(receiverId);
+		const sender = await User.findById(senderId);
+
+		sender.sentFriendRequests.pull({
+			user: receiverId,
+		});
+
+		receiver.receivedFriendRequests.pull({
+			user: senderId,
+		});
+
+		await sender.save();
+		await receiver.save();
+
+		res.status(200).json({ message: "Friend request deleted successfully" });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+}
+
+//This gets receieved friend requests data
+async function getFriendRequests(req, res) {
+	try {
+		const userId = req.user_id;
+		const user = await User.findById(userId).populate(
+			"receivedFriendRequests.user",
+		);
+		const userFriendrequests = user.receivedFriendRequests;
+		res.status(200).json({
+			friendRequests: userFriendrequests,
+		});
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+}
+
+async function getFriends(req, res) {
+	try {
+		const userId = req.user_id;
+		const user = await User.findById(userId).populate("friends");
+		const userFriends = user.friends;
+		res.status(200).json({
+			friends: userFriends,
+		});
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+}
+
+//This function is getting users for people you may know section
+// Users gotten are not in the friends array and also in the sent or received friend requests array
+async function getOtherUsers(req, res) {
+	try {
+		const userId = req.user_id;
+		const user = await User.findById(userId);
+		const userFriendsId = user.friends;
+		const userSentRequestId = user.sentFriendRequests.map(
+			(request) => request.user,
+		);
+		const userReceivedRequestId = user.receivedFriendRequests.map(
+			(request) => request.user,
+		);
+
+		const excludedIds = [
+			...userFriendsId,
+			...userSentRequestId,
+			...userReceivedRequestId,
+			userId,
+		];
+
+		const otherUsers = await User.find({ _id: { $nin: excludedIds } });
+		res.status(200).json({
+			otherUsers: otherUsers,
+		});
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+}
+
+//This function removes friends from friends array of users
+async function removeFriends(req, res) {
+	try {
+		const removerId = req.user_id;
+		const removedId = req.params.id;
+
+		const removerUser = await User.findById(removerId);
+		const removedUser = await User.findById(removedId);
+
+		removerUser.friends.pull(removedId);
+
+		removedUser.friends.pull(removerId);
+
+		await removerUser.save();
+		await removedUser.save();
+
+		res.status(200).json({ message: "Friend removed successfully" });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+}
+
+const FriendsController = {
+	addFriendRequest: addFriendRequest,
+	getFriendRequests: getFriendRequests,
+	getFriends: getFriends,
+	getOtherUsers: getOtherUsers,
+	acceptFriendRequest: acceptFriendRequest,
+	deleteFriendRequest: deleteFriendRequest,
+	removeFriend: removeFriends
+};
+
+module.exports = FriendsController;
