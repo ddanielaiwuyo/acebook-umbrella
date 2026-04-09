a6e4a8a81e241042aeaef855806285bef111cdf2
Ameerah Adisa <ameeygold@gmail.com>
2026-04-09 18:15:50 +0100

    Updated the accept and reject friend request buttons component

--- frontend/src/components/RequestButtons.jsx
+++ frontend/src/components/RequestButtons.jsx
@@ -1,8 +1,20 @@
-export function RequestButtons() {
-    return (
-        <div>
-            <button>Accept</button>
-            <button>Delete</button>
-        </div>
-    )
+import { acceptFriendRequest, deleteFriendRequest } from "../services/friends";
+
+export function RequestButtons(props) {
+	async function handleAcceptButtonClick() {
+		await acceptFriendRequest(props.senderId);
+		props.onAction(props.senderId, "accept");
+	}
+
+	async function handleDeleteButtonClick() {
+		await deleteFriendRequest(props.senderId);
+		props.onAction(props.senderId, "delete");
+	}
+
+	return (
+		<div>
+			<button onClick={handleAcceptButtonClick}>Accept</button>
+			<button onClick={handleDeleteButtonClick}>Delete</button>
+		</div>
+	);
 }
