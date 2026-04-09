8fb7a6ca71ee6a30030d435f81c0b088d9986a74
ddanielaiwuyo <aiwuyoeghosadaniel2@gmail.com>
2026-04-09 17:17:26 +0100

    test: added tests for authentication.js

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
 const EMAIL_MIN_LENGTH = 5;
@@ -6,58 +6,67 @@ const PASSWORD_MIN_LENGTH = 8;
 const SALT_ROUNDS = 10;
 
 async function create(req, res) {
-	try {
-		const { firstName, lastName, email, password, } = req.body
-		const user = new User({ email, password });
-		if (!email || !password) {
-			res.status(400).json({ message: "Invalid credentials, must contain email and password " })
-			return
-		}
+  try {
+    const { firstName, lastName, email, password } = req.body;
+    if (!email || !password) {
+      res.status(400).json({
+        message: "Invalid credentials, must contain email and password ",
+      });
+      return;
+    }
 
-		if (!emailIsValid(email)) {
-			res.status(400).json({ message: "Invalid email address" })
-			return
-		}
+    if (!emailIsValid(email)) {
+      res.status(400).json({ message: "Invalid email address" });
+      return;
+    }
 
-		if (password.trim().length < PASSWORD_MIN_LENGTH) {
-			console.log("password too short", password, password.length)
-			res.status(400).json({ message: `Password too short, must be at least ${PASSWORD_MIN_LENGTH}` })
-			return
-		}
+    if (password.trim().length < PASSWORD_MIN_LENGTH) {
+      console.log("password too short", password, password.length);
+      res.status(400).json({
+        message: `Password too short, must be at least ${PASSWORD_MIN_LENGTH}`,
+      });
+      return;
+    }
 
-		// Now check if user with that email already exists
-		const userExists = await User.findOne({ email: email })
-		if (userExists) {
-			console.log(`User with email: ${email} already exists `)
-			res.status(409).json({ message: `User with email ${email} already exists` })
-			return
-		}
+    // Now check if user with that email already exists
+    const userExists = await User.findOne({ email: email });
+    if (userExists) {
+      console.log(`User with email: ${email} already exists `);
+      res
+        .status(409)
+        .json({ message: `User with email ${email} already exists` });
+      return;
+    }
 
-		const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
-		const newUser = new User({ email: email, password: hashedPassword })
-		await newUser.save()
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+    const newUser = new User({ email: email, password: hashedPassword });
+    await newUser.save();
 
-		console.info(`User with email: ${email} saved successfully`)
-		res.status(201).json({ message: "Created successfully" })
-
-	} catch (err) {
-		console.error("Could not create user")
-		console.error(err)
-		res.status(500).json({ message: "Service is down, please try again later" });
-	}
+    console.info(`User with email: ${email} saved successfully`);
+    res.status(201).json({ message: "Created successfully" });
+  } catch (err) {
+    console.error("Could not create user");
+    console.error(err);
+    res
+      .status(500)
+      .json({ message: "Service is down, please try again later" });
+  }
 }
 
 function emailIsValid(email) {
-	let formattedEmail = email.trim().toLowerCase()
-	if (formattedEmail.length >= EMAIL_MIN_LENGTH && formattedEmail.includes("@")) {
-		return true
-	}
+  let formattedEmail = email.trim().toLowerCase();
+  if (
+    formattedEmail.length >= EMAIL_MIN_LENGTH &&
+    formattedEmail.includes("@")
+  ) {
+    return true;
+  }
 
-	return false
+  return false;
 }
 
 const UsersController = {
-	create: create,
+  create: create,
 };
 
 module.exports = UsersController;
