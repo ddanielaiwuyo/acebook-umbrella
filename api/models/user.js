4af01a816a10103904d2d820991df5231ea5c5c5
Ameerah Adisa <ameeygold@gmail.com>
2026-04-10 11:11:57 +0100

    Made firstName and lastName fields not required

--- api/models/user.js
+++ api/models/user.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-	firstName: { type: String, required: true },
-	lastName: { type: String, required: true },
+	firstName: { type: String, required: false },
+	lastName: { type: String, required: false },
 	profileImg: { type: String },
 	email: { type: String, required: true },
 	password: { type: String, required: true },
