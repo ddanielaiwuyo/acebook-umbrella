32180318432dd870388fae33fa0f0775123157d4
Sajif Devapalan <sajifdevapalan@outlook.com>
2026-04-10 15:23:59 +0100

    used onClick to add functionality on profile page to click on friends on friends list

--- frontend/src/components/Profile/FriendList.jsx
+++ frontend/src/components/Profile/FriendList.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./FriendList.css";
+import { useNavigate } from "react-router-dom";
 
 const FriendList = ({ friends }) => {
+  const navigate = useNavigate();
   return (
     <div className="friend-list">
       <h3>Friends</h3>
       <div className="friend-grid">
         {friends.map((friend) => (
-          <div key={friend._id} className="friend-item">
+          <div key={friend._id} className="friend-item" onClick={() => navigate(`/profile/${friend._id}`)} style={{ cursor: "pointer" }}>
             <img
               src={
                 friend.profilePic ||
