b9f97e15188afaa3465356b36c6965058ab52232
ddanielaiwuyo <aiwuyoeghosadaniel2@gmail.com>
2026-04-13 12:07:31 +0100

    feat: removed /posts from react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,10 +23,6 @@ const router = createBrowserRouter([
     element: <SignupPage />,
   },
   {
-    path: "/posts",
-    element: <FeedPage />,
-  },
-  {
     path: "/feed",
     element: <FeedPage />,
   },
