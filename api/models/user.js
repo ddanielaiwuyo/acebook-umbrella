23b7353600df05a94e27595e3bde82baa4a72398
Sajif Devapalan <sajifdevapalan@outlook.com>
2026-04-13 13:38:48 +0100

    conflicts corrected

--- api/models/user.js
+++ api/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
+  firstName: { type: String, required: false },
+  lastName: { type: String, required: false },
   email: { type: String, required: true },
   password: { type: String, required: true },
   name: { type: String },
@@ -8,13 +10,42 @@ const UserSchema = new mongoose.Schema({
   bio: { type: String },
   location: { type: String },
   work: { type: String },
-  bio: { type: String },
   birthday: { type: String },
   joined: { type: String },
 
   friends: [
-    { type: mongoose.Schema.Types.ObjectId, ref: "User"}
-  ]
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  sentFriendRequests: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+      status: {
+        type: String,
+        enum: ["pending", "accepted", "rejected"],
+        default: "pending",
+      },
+    },
+  ],
+  receivedFriendRequests: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+      status: {
+        type: String,
+        enum: ["pending", "accepted", "rejected"],
+        default: "pending",
+      },
+    },
+  ],
+
 });
 
 const User = mongoose.model("User", UserSchema);
