fdd3cc1a862fa8ae2c9427f6f8179446dae0c859
Sajif Devapalan <sajifdevapalan@outlook.com>
2026-04-14 12:02:05 +0100

    fixed errors relating to tokens

--- api/routes/profile.js
+++ api/routes/profile.js
@@ -11,35 +11,39 @@ router.get("/:userId", async (req, res) => {
     // Gets user and adds the friends
     const user = await User.findById(userId).populate(
       "friends",
-      "name profilePic"
+      "firstName lastName profilePic"
     );
 
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(404).json({ok: false, message: "User not found" });
     }
 
     // Get the user's posts only
     const posts = await Post.find({ owner: userId })
       .sort({ createdAt: -1 })
-      .populate("owner", "name profilePic");
+      .populate("owner", "firstName lastName profilePic");
 
     //combined profile data of user only
-    res.json({
-      _id: user._id,
-      name: user.name,
-      profilePic: user.profilePic,
-      bio: user.bio,
-      location: user.location,
-      work: user.work,
-      birthday: user.birthday,
-      joined: user.joined,
-      friends: user.friends,
-      posts: posts,
+    res.status(200).json({
+      ok: true, message: "OK", data: {
+        _id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        profilePic: user.profilePic,
+        bio: user.bio,
+        location: user.location,
+        work: user.work,
+        birthday: user.birthday,
+        joined: user.joined,
+        friends: user.friends,
+        posts: posts,
+      },
     });
 
   } catch (err) {
+    console.error("Could not get user profile", err)
     console.error(err);
-    res.status(500).json({ message: "Server error" });
+    res.status(500).json({ok: false,  message: "Service is down, please try again later" });
   }
 });
 
