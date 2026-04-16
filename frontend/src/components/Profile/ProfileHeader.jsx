10db3b6ab4c159c40edaee1583d2aa41923a0351
Ameerah Adisa <ameeygold@gmail.com>
2026-04-16 11:19:05 +0100

    Fixed error from merging changes from main in profile header component

--- frontend/src/components/Profile/ProfileHeader.jsx
+++ frontend/src/components/Profile/ProfileHeader.jsx
@@ -7,26 +7,35 @@ const ProfileHeader = ({ name, profilePic, isOwner, otherUserId }) => {
 
 	return (
 		<div className="profile-header">
-			<div className="avatar-container">
+			<div className="profile-banner">
 				<img
-					src={
-						profilePic ||
-						`https://api.dicebear.com/7.x/adventurer/svg?seed=${name}`
-					}
-					alt={name}
-					className="profile-avatar"
+					src={`https://api.dicebear.com/9.x/glass/svg?seed=YOURSEED${name}-banner`}
+					alt="banner"
+					className="banner-img"
 				/>
 			</div>
-			<h2 className="profile-name">{name}</h2>
-			{isOwner ? (
-				<button className="edit-profile-btn">Edit Profile</button>
-			) : (
-				<button
-					className="message-btn"
-					onClick={() => navigate(`/messages/${otherUserId}`)}>
-					Message
-				</button>
-			)}
+			<div className="profile-content">
+				<div className="avatar-container">
+					<img
+						src={
+							profilePic ||
+							`https://api.dicebear.com/7.x/adventurer/svg?seed=${name}`
+						}
+						alt={name}
+						className="profile-avatar"
+					/>
+				</div>
+				<h2 className="profile-name">{name}</h2>
+				{isOwner ? (
+					<button className="edit-profile-btn">Edit Profile</button>
+				) : (
+					<button
+						className="message-btn"
+						onClick={() => navigate(`/messages/${otherUserId}`)}>
+						Message
+					</button>
+				)}
+			</div>
 		</div>
 	);
 };
