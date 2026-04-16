ba3dcaf730de708e8dd8951f30976c056888a874
ddanielaiwuyo <aiwuyoeghosadaniel2@gmail.com>
2026-04-16 11:01:37 +0100

    test: fixed authentication tests

--- frontend/tests/services/authentication.test.js
+++ frontend/tests/services/authentication.test.js
@@ -9,114 +9,115 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 createFetchMock(vi).enableMocks();
 
 describe("authentication service", () => {
-  describe("login", () => {
-    test("calls the backend url for a token", async () => {
-      const testEmail = "test@testEmail.com";
-      const testPassword = "12345678";
+	describe("login", () => {
+		test("calls the backend url for a token", async () => {
+			const testEmail = "test@testEmail.com";
+			const testPassword = "12345678";
 
-      fetch.mockResponseOnce(JSON.stringify({ token: "testToken" }), {
-        status: 201,
-      });
+			fetch.mockResponseOnce(JSON.stringify({ message: "OK", token: "testToken" }), {
+				status: 201,
+			});
 
-      await login(testEmail, testPassword);
+			await login(testEmail, testPassword);
 
-      // This is an array of the arguments that were last passed to fetch
-      const fetchArguments = fetch.mock.lastCall;
-      const url = fetchArguments[0];
-      const options = fetchArguments[1];
+			// This is an array of the arguments that were last passed to fetch
+			const fetchArguments = fetch.mock.lastCall;
+			const url = fetchArguments[0];
+			const options = fetchArguments[1];
 
-      expect(url).toEqual(`${BACKEND_URL}/tokens`);
-      expect(options.method).toEqual("POST");
-      expect(options.body).toEqual(
-        JSON.stringify({ email: testEmail, password: testPassword }),
-      );
-      expect(options.headers["Content-Type"]).toEqual("application/json");
-    });
+			expect(url).toEqual(`${BACKEND_URL}/tokens`);
+			expect(options.method).toEqual("POST");
+			expect(options.body).toEqual(
+				JSON.stringify({ email: testEmail, password: testPassword }),
+			);
+			expect(options.headers["Content-Type"]).toEqual("application/json");
+		});
 
-    test("returns the token if the request was a success", async () => {
-      const testEmail = "test@testEmail.com";
-      const testPassword = "12345678";
+		test("returns the token if the request was a success", async () => {
+			const testEmail = "test@testEmail.com";
+			const testPassword = "12345678";
 
-      fetch.mockResponseOnce(JSON.stringify({ token: "testToken" }), {
-        status: 201,
-      });
+			fetch.mockResponseOnce(JSON.stringify({ message: "OK", token: "testToken" }), {
+				status: 201,
+			});
 
-      const token = await login(testEmail, testPassword);
-      expect(token).toEqual("testToken");
-    });
+			const response = await login(testEmail, testPassword);
+			expect(response.token).toEqual("testToken");
+		});
 
-    test("throws an error if the request failed", async () => {
-      const testEmail = "test@testEmail.com";
-      const testPassword = "12345678";
+		// When a user signups successfully, no data is sent  back to them
+		// Only a message in the response body that says `Created Successfully`
+		describe("signup", () => {
+			test("calls the backend url for a token", async () => {
+				const testEmail = "test@testEmail.com";
+				const testPassword = "12345678";
+				const payload = {
+					firstName: "Test",
+					lastName: "User",
+					email: testEmail,
+					password: testPassword,
+				}
 
-      fetch.mockResponseOnce(JSON.stringify({ message: "Wrong Password" }), {
-        status: 403,
-      });
+				fetch.mockResponseOnce(JSON.stringify(""), {
+					status: 201,
+					message: "Created Successfully",
+				});
 
-      try {
-        await login(testEmail, testPassword);
-      } catch (err) {
-        expect(err.message).toEqual(
-          "Received status 403 when logging in. Expected 201",
-        );
-      }
-    });
-  });
+				await signup(payload);
 
-  describe("signup", () => {
-    test("calls the backend url for a token", async () => {
-      const testEmail = "test@testEmail.com";
-      const testPassword = "12345678";
+				// This is an array of the arguments that were last passed to fetch
+				const fetchArguments = fetch.mock.lastCall;
+				const url = fetchArguments[0];
+				const options = fetchArguments[1];
 
-      fetch.mockResponseOnce("", {
-        status: 201,
-      });
 
-      await signup(testEmail, testPassword);
+				expect(url).toEqual(`${BACKEND_URL}/users`);
+				expect(options.method).toEqual("POST");
+				expect(JSON.parse(options.body)).toEqual(payload);
+				expect(options.headers["Content-Type"]).toEqual("application/json");
+			});
 
-      // This is an array of the arguments that were last passed to fetch
-      const fetchArguments = fetch.mock.lastCall;
-      const url = fetchArguments[0];
-      const options = fetchArguments[1];
+			test("returns nothing if the signup request was a success", async () => {
+				const testEmail = "test@testEmail.com";
+				const testPassword = "12345678";
 
-      expect(url).toEqual(`${BACKEND_URL}/users`);
-      expect(options.method).toEqual("POST");
-      expect(options.body).toEqual(
-        JSON.stringify({ email: testEmail, password: testPassword }),
-      );
-      expect(options.headers["Content-Type"]).toEqual("application/json");
-    });
+				const payload = {
+					firstName: "Test",
+					lastName: "User",
+					email: testEmail,
+					password: testPassword,
+				}
 
-    test("returns nothing if the signup request was a success", async () => {
-      const testEmail = "test@testEmail.com";
-      const testPassword = "12345678";
+				fetch.mockResponseOnce(JSON.stringify(""), {
+					status: 201,
+					message: "Created Successfully",
+				});
 
-      fetch.mockResponseOnce(JSON.stringify(""), {
-        status: 201,
-      });
+				const response = await signup(payload);
+				expect(response.status).toEqual(201)
+				console.log(response)
+				expect(response.token).toEqual(undefined);
+			});
 
-      const token = await signup(testEmail, testPassword);
-      expect(token).toEqual(undefined);
-    });
+			test("throws an error if the request failed", async () => {
+				const testEmail = "test@testEmail.com";
+				const testPassword = "12345678";
 
-    test("throws an error if the request failed", async () => {
-      const testEmail = "test@testEmail.com";
-      const testPassword = "12345678";
+				fetch.mockResponseOnce(
+					JSON.stringify({ message: "User already exists" }),
+					{
+						status: 400,
+					},
+				);
 
-      fetch.mockResponseOnce(
-        JSON.stringify({ message: "User already exists" }),
-        {
-          status: 400,
-        },
-      );
-
-      try {
-        await signup(testEmail, testPassword);
-      } catch (err) {
-        expect(err.message).toEqual(
-          "Received status 400 when signing up. Expected 201",
-        );
-      }
-    });
-  });
+				try {
+					await signup(testEmail, testPassword);
+				} catch (err) {
+					expect(err.message).toEqual(
+						"Received status 400 when signing up. Expected 201",
+					);
+				}
+			})
+		});
+	});
 });
